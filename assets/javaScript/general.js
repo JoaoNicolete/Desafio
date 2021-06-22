123d04c0999726d170f3af4b0eadd28e339420c6
João Nicolete <joao.nicolete@usp.br>
2021-06-22 11:09:14 -0300

    adicionando hoover na tela de opção

--- assets/javaScript/general.js
+++ assets/javaScript/general.js
@@ -1,6 +1,8 @@
-// variáveis para login
+// variáveis
 let email = document.querySelector('#Email')
 let senha = document.querySelector('#senha')
+let img1 = document.querySelector('.imagem')
+let img2 = document.querySelector('.imagem2')
 
 // validação do campo email
 
@@ -15,3 +17,26 @@ function validaEmail(){
 	}
 }
 
+// função que diminui a imagem
+function imgHoover(){
+	img1.style.width = '75%'
+	img1.style.height = '75%'
+}
+
+// função que volta a imagem ao normal
+function imgNormal(){
+	img1.style.width = '100%'
+	img1.style.height = '100%'
+}
+
+// função que diminui a imagem
+function imgHoover2(){
+	img2.style.width = '75%'
+	img2.style.height = '75%'
+}
+
+// função que volta a imagem ao normal
+function imgNormal2(){
+	img2.style.width = '100%'
+	img2.style.height = '100%'
+}
